d2e278130d947cec75cc7303ec45dd923a5ee01d
Nate <nate@idontchop.com>
2021-09-13 01:17:59 -0700

    Added Content Tester Tips

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+npm run build
+ssh jeff@192.168.1.55 "rm -rf /home/jeff/lovemire.com/*"
+scp -r build/* jeff@192.168.1.55:/home/jeff/lovemire.com
---
+++ src/accessLevelCustomCards/ContentTester.js
@@ -0,0 +1,105 @@
+import styled from 'styled-components'
+import React, {useEffect, useState} from 'react';
+import { LinearProgress, Button, Label, TextField, Card, Typography, CardContent, CardActions, InputAdornment, Link} from '@material-ui/core'
+import Grid from '@material-ui/core/Grid';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Paper from '@material-ui/core/Paper';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import {useSpring, animated} from 'react-spring'
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const ContentTester = (props) => {
+
+    const tutorialSteps = [
+        {
+          label: 'Navigate to My Profile',
+          imgPath:
+            '/images/MyProfile.png',
+        },
+        {
+            label: 'Touch Add Post Button',
+            imgPath:
+              '/images/AddPost.png',
+        },
+        {
+            label: 'Add a pic of yourself!',
+            imgPath:
+              '/images/FirstImage.png',
+        },
+        {
+            label: 'Add a second image!',
+            imgPath:
+              '/images/SecondImage.png',
+        },
+        {
+            label: 'Add a provacative secret image!',
+            imgPath:
+              '/images/SecretImage.png',
+        },
+        {
+            label: 'Adjust the preview blur and click Save Secret',
+            imgPath:
+              '/images/SaveSecret.png',
+        },
+        {
+            label: 'Remember to hit Post Live!',
+            imgPath:
+              '/images/PostLive.png',
+        }
+      ];
+
+      const [activeStep, setActiveStep] = useState(0);
+      const maxSteps = tutorialSteps.length;
+
+      const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+      };
+    
+      const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+      };
+
+    return (<Grid item>
+        <animated.div style={props.springStyles}>
+        <Card className={props.theme.wideCard} style={{backgroundColor: "rgba(255, 247, 247, 0.6)"}}>
+        <CardContent>
+          <Typography gutterBottom variant="h5">Content Tester Tips</Typography>
+          <Typography gutterBottom>Thank you for helping us test by providing content!</Typography>
+          <Typography gutterBottom>Primarily, we need attractive posts that might assist us to see how individual users might browse posts.</Typography>
+        </CardContent>
+
+        <MobileStepper
+            steps={maxSteps}
+            position="static"
+            variant="text"
+            activeStep={activeStep}
+            nextButton={
+            <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+                Next
+            </Button>
+            }
+            backButton={
+            <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                Back
+            </Button>
+            }
+        />
+        <CardContent>
+            <Paper square elevation={0}>
+                <Typography variant="h5">{tutorialSteps[activeStep].label}</Typography>
+            </Paper>
+            <img style={{width: "auto", height: "400px", margin: "auto", display: "block"}}
+                src={tutorialSteps[activeStep].imgPath}
+            />
+        </CardContent>
+
+        </Card>
+        </animated.div>
+        </Grid>)
+}
+
+export default ContentTester;
