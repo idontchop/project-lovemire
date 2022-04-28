6295bbbfe73c84fa2d5ac5ab2f9e82b86f50e8a1
Nate <nate@idontchop.com>
2022-04-28 11:38:49 -0700

    Presentation work

--- src/components/presenation/LoveMire.js
+++ src/components/presenation/LoveMire.js
@@ -20,13 +20,6 @@ const Slide = styled.div`
         box-shadow: 0px 0px 14px 9px #707070;  
     }
 
-    div {
-        height: 100%;
-
-        div {
-            height: auto;
-        }
-    }
 `
 
 const Bubbble = styled.div`
@@ -39,19 +32,20 @@ const Bubbble = styled.div`
     margin: auto;
 `
 
+
 export const LoveMire = (props) => {
 
 
     return <>
         {!props.slide && <Slide>{props.children}</Slide>}
         {props?.slide === "1" && <Slide>
-            <div className="sectionBody d-flex justify-content-center align-items-stretch flex-column">
+            <div style={{height: '100%'}} className="sectionBody d-flex justify-content-center align-items-stretch flex-column">
                 <h1>Project: Lovemire</h1>
                 <h4>Production name: Flirvy</h4>
             </div>
             </Slide>}
         {props?.slide === "2" && <Slide>
-            <div className="sectionBodyInsert d-flex flex-grow-1 ">
+            <div  style={{height: '100%'}}  className="sectionBodyInsert d-flex flex-grow-1 ">
                 <Bubbble>
                 <h3>A Free Dating/Social App</h3>
                 <p>Combines a dating app and a media browsing site.</p>
@@ -63,7 +57,7 @@ export const LoveMire = (props) => {
             
             </Slide>}    
         {props?.slide === "3" && <Slide>
-        <div className="sectionBodyInsert d-flex flex-grow-1 ">
+        <div  style={{height: '100%'}} className="sectionBodyInsert d-flex flex-grow-1 ">
                 <Bubbble>
                 <h3>Request a Test Account!</h3>
                 <ul>
@@ -73,12 +67,20 @@ export const LoveMire = (props) => {
                 </ul>
                 </Bubbble>
             </div>
-            <div className="sectionBodyInsert d-flex flex-grow-1 align-items-center justify-content-center"><img className="img-fluid" src={LoveMire2} /></div>
+            <div  style={{height: '100%'}} className="sectionBodyInsert d-flex flex-grow-1 align-items-center justify-content-center"><img className="img-fluid" src={LoveMire2} /></div>
             </Slide>}    
         {props?.slide === "4" && <Slide>
-            <div className="sectionBody d-flex justify-content-center align-items-stretch flex-column">
+            <div  style={{height: '100%'}} className="sectionBody d-flex justify-content-center align-items-stretch flex-column">
                 <h1>Target Public Beta: </h1>
                 <h4>Winter 2022</h4>
+                <div style={{left: '50%', transform: 'translateX(-50%)',
+                    position: 'absolute', bottom: '10px',
+                    cursor: 'pointer'}}>
+                <h5 className="inkfree" 
+                    style={{display: "inline-block", 
+                    margin: "0 25px"}}>Current Principals</h5>
+                <div className="scroll-down"></div>
+                </div>
             </div>
             </Slide>}                              
     </>
